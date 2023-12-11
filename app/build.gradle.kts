87933519c5c15600b018c90e0b1e6db141d865ce
Nintsa <nintsa.kemularia@gmail.com>
2023-12-11 08:15:03 +0400

    still not works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,25 +40,19 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    implementation("androidx.privacysandbox.plugins:plugins-privacysandbox-library:1.0.0-alpha02")
 
-    implementation("com.google.android.material:material:1.6.1") // Updated version
-    implementation("androidx.privacysandbox.plugins:plugins-privacysandbox-library:1.0.0-beta01") // Updated version
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
     implementation("com.squareup.moshi:moshi:1.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation ("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+
 }
--- app/src/main/java/com/example/assignment16/WelcomeFragment.kt
+++ app/src/main/java/com/example/assignment16/WelcomeFragment.kt
@@ -1,11 +1,6 @@
 package com.example.assignment16
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import com.example.assignment16.base.BaseFragment
 import com.example.assignment16.databinding.FragmentWelcomeBinding
 
 
